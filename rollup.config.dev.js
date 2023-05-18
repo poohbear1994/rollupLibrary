d0625a3476d8e951d326896ff700a840cb07ec1e
jianxin.lu <jianxin.lu@geely.com>
2023-05-18 14:51:24 +0800

    build: pkg依赖升级

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -1,19 +1,16 @@
-/* eslint-disable */
-import path from 'path'
-import resolve from 'rollup-plugin-node-resolve'
+import { fileURLToPath } from 'node:url'
+import resolve from '@rollup/plugin-node-resolve'
 import { babel } from '@rollup/plugin-babel'
-import json from 'rollup-plugin-json'
-import vue from 'rollup-plugin-vue'
-import postcss from 'rollup-plugin-postcss'
+import json from '@rollup/plugin-json'
+import commonjs from '@rollup/plugin-commonjs'
+import terser from '@rollup/plugin-terser'
 import ts from 'rollup-plugin-typescript2'
-import commonjs from 'rollup-plugin-commonjs'
-import { terser } from 'rollup-plugin-terser'
+import vue from '@vitejs/plugin-vue'
+import postcss from 'rollup-plugin-postcss'
 
-const inputPath = path.resolve(__dirname, './src/index.ts')
-const distPath = path.resolve(__dirname, './dist')
-const outputPath = path.resolve(__dirname, './dist/index.umd.js')
-const cjsOutputPath = path.resolve(__dirname, './dist/index.cjs.js')
-const esOutputPath = path.resolve(__dirname, './dist/index.es.js')
+const inputPath = fileURLToPath(new URL('src/index.ts', import.meta.url))
+const esOutputPath = fileURLToPath(new URL('dist/index.es.js', import.meta.url))
+const tsConfigPath = fileURLToPath(new URL('tsconfig.json', import.meta.url))
 
 // node环境默认不识别ESM
 export default {
@@ -23,32 +20,11 @@ export default {
 	output: [
 		{
 			// 指定文件打包输出路径
-			file: outputPath,
-			// 指定文件打包输出模块格式
-			format: 'umd',
-			// 指定文件打包输出的名字
-			name: 'index',
-			globals: { vue: 'Vue' },
-			exports: 'named',
-		},
-		{
-			// 指定文件打包输出路径
-			file: cjsOutputPath,
-			// 指定文件打包输出模块格式
-			format: 'cjs',
-			// 指定文件打包输出的名字
-			name: 'index',
-			globals: { vue: 'Vue' },
-			exports: 'named',
-		},
-		{
-			// 指定文件打包输出路径
 			file: esOutputPath,
 			// 指定文件打包输出模块格式
 			format: 'es',
 			// 指定文件打包输出的名字
 			name: 'index',
-			global: { vue: 'Vue' },
 			exports: 'named',
 		},
 	],
@@ -60,14 +36,14 @@ export default {
 		}),
 		commonjs(),
 		ts({
-			tsconfig: path.resolve(__dirname, 'tsconfig.json'),
+			tsconfig: tsConfigPath,
 		}),
 		// 将ES6语法转换为ES5语法
 		babel({ babelHelpers: 'bundled', extensions: ['.ts'] }),
 		json(),
 		vue(),
 		postcss({ plugins: [] }),
-		// terser()
+		terser(),
 	],
 	// 不需要打包的模块
 	external: ['vue', 'lodash', 'element-plus', '@element-plus/icons-vue'],
